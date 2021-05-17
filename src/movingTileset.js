6bbc727a6b06ed3502e6bd10ba365629fae2c702
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-05-17 23:34:20 +0300

    add wizard sprite, start collisions

--- src/movingTileset.js
+++ src/movingTileset.js
@@ -1,6 +1,8 @@
-import tile from './assets/Wasteland_Sky.png';
-import planeSprite from './assets/plane/plane.png';
-import planeJSON from './assets/plane/plane.json';
+import tile from "./assets/Wasteland_Sky.png";
+import planeSprite from "./assets/plane/plane.png";
+import planeJSON from "./assets/plane/plane.json";
+import wizardSprite from "./assets/wizard/wizard.png";
+import wizardJSON from "./assets/wizard/wizard.json";
 
 export const movingTileset = () => {
 	const phaserConfig = {
@@ -13,24 +15,24 @@ export const movingTileset = () => {
 			init: initScene,
 			preload: preloadScene,
 			create: createScene,
-			update: updateScene
-		}
+			update: updateScene,
+		},
 	};
-	
+
 	const game = new Phaser.Game(phaserConfig);
-	
-	let sky, cloudsWhiteSmall, cursors,plane;
-	
-	function initScene() { }
+
+	let sky, wizard, cursors, plane;
+
+	function initScene() {}
 	function preloadScene() {
 		this.load.image("sky", tile);
-    	this.load.image("clouds-white-small", tile);
+		this.load.atlas("wizard", wizardSprite, wizardJSON);
 		this.load.atlas("plane", planeSprite, planeJSON);
 	}
+
 	function createScene() {
-		sky = this.add.tileSprite(512,256, 512, 256, "sky");
-		sky.setScale(2)
-    	// cloudsWhiteSmall = this.add.tileSprite(640, 200, 1280, 400, "clouds-white-small");
+		sky = this.add.tileSprite(512, 256, 512, 256, "sky");
+		sky.setScale(2);
 
 		this.anims.create({
 			key: "fly",
@@ -40,11 +42,11 @@ export const movingTileset = () => {
 				suffix: ".png",
 				start: 1,
 				end: 3,
-				zeroPad: 1
+				zeroPad: 1,
 			}),
-			repeat: -1
+			repeat: -1,
 		});
-	
+
 		this.anims.create({
 			key: "explode",
 			frameRate: 7,
@@ -53,41 +55,59 @@ export const movingTileset = () => {
 				suffix: ".png",
 				start: 1,
 				end: 3,
-				zeroPad: 1
+				zeroPad: 1,
 			}),
-			repeat: 2
+			repeat: 2,
 		});
 
+		this.anims.create({
+			key: 'idle',
+			frameRate: 40,
+			frames: this.anims.generateFrameNames('wizard', {
+				prefix: 'Chara - BlueIdle',
+				suffix : '.png',
+				start : 1,
+				end: 19,
+				zeroPad: 5,
+			}),
+			repeat: -1,
+		})
+
 		console.log(this);
-		plane = this.add.sprite(222,222, "plane");
-		plane.setScale(0.2)
+		plane = this.add.sprite(222, 222, "plane");
+		plane.setScale(0.2);
 		plane.play("fly");
+
+		wizard = this.add.sprite(500, 360, "wizard");
+		wizard.play('idle')
+		
+		// destroing by timer
+		// this.time.addEvent({
+		// 	delay: 3000,
+		// 	callback: () => {
+		// 		plane.play("explode");
+		// 		plane.once(Phaser.Animations.Events.SPRITE_ANIMATION_COMPLETE, () => {
+		// 			plane.destroy();
+		// 		});
+		// 	},
+		// });
 
 		cursors = this.input.keyboard.createCursorKeys();
 	}
 	function updateScene() {
 		sky.tilePositionX += 0.3;
 
-		if (cursors.left.isDown)
-        {
-			console.log(plane)
-			plane.x -= 3
+		if (cursors.left.isDown) {
+			console.log(plane);
+			plane.x -= 3;
 
-            // plane.anims.play('fly', true);
-        }
-        else if (cursors.right.isDown)
-        {
-            plane.x += 3
-        }
-		else if (cursors.up.isDown)
-        {
-            plane.y -= 3
-        }
-		else if (cursors.down.isDown)
-        {
-            plane.y += 3
-        }
-
+			// plane.anims.play('fly', true);
+		} else if (cursors.right.isDown) {
+			plane.x += 3;
+		} else if (cursors.up.isDown) {
+			plane.y -= 3;
+		} else if (cursors.down.isDown) {
+			plane.y += 3;
+		}
 	}
-}
-
+};
