02953fe2d72fecdd476c1c34ec5f1dd8449f7926
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-05-23 23:42:09 +0300

    nothing special

--- src/endless cave/scenes/play.js
+++ src/endless cave/scenes/play.js
@@ -4,10 +4,31 @@ class Play extends Phaser.Scene {
 	}
 
 	init() {
-		
+		this.CONFIG = this.sys.game.CONFIG;
+
+		this.DEPTH = {
+			floor:0
+		}
+
+		this.allow_input = false;
+		this.is_pause = false;
+		this.is_gameover = false;
+
 	}
 
 	create() {
 
+	}
+
+	update() {
+
+	}
+
+	updateCamera() {
+
+	}
+
+	setCamSpeed() {
+		
 	}
 }
